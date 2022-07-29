103a9306e971cb378323e0b0740542f72a4adb5e
Ennis Kurt <46458661+ennis-kurt@users.noreply.github.com>
2022-07-28 20:06:18 -0400

    Update ensemble_run.sh

--- main/ensemble_run.sh
+++ main/ensemble_run.sh
@@ -1,15 +1,29 @@
 : <<"END"
 # ------------------------------------------------------------------------------------------------------------------------------------------
 The MSTM code already have multiple runs and parallel processing capability. However, these features are limited. 
-Here, I wrote a bash script ( esnsemble_run.sh and its other versions) to automate the multiple MSTM run executions using multiple cpu-cores serially. Original MSTM code can execute a single run with parallel processing, but when several multiple runs needed then its parallel processing is much less efficient than executing each run on a single cpu core. In addition to this, during the calculation of phase metrices, t-matrix is stored to memory and if the run is executed on multiple core using parallel processing for each cpu a copy of t-matrix run is stored on memory. This can easily exhaust memory especially if run on a personal computer. If only a limited number of runs needed then parallel processing is still benefitial. 
+Here, I wrote a bash script ( esnsemble_run.sh and its other versions) to automate the multiple MSTM run executions 
+using multiple cpu-cores serially. Original MSTM code can execute a single run with parallel processing, 
+but when several multiple runs needed then its parallel processing is much less efficient than executing each run 
+on a single cpu core. In addition to this, during the calculation of phase metrices, t-matrix is stored to memory 
+and if the run is executed on multiple core using parallel processing for each cpu a copy of t-matrix run is stored 
+on memory. This can easily exhaust memory especially if run on a personal computer. If only a limited number of runs 
+needed then parallel processing is still benefitial. 
 
-However, when tens of multiple runs are needed dedicated each cpu core to a single run at the same time while using multiple cores is much more efficient in terms of both run time and memory usage. The ensemble_run.sh script does this automatically, along with other things. 
+However, when tens of multiple runs are needed dedicated each cpu core to a single run at the same time while using 
+multiple cores is much more efficient in terms of both run time and memory usage. The ensemble_run.sh script does 
+this automatically, along with other things. 
 
-Another issue with MSTM code when it comes to multiple runs is that the original code provides only a limited way to select the run paramaters. ensemble_run script extends its multiple run features. 
+Another issue with MSTM code when it comes to multiple runs is that the original code provides only a limited way to 
+select the run paramaters. ensemble_run script extends its multiple run features. 
 
-For instance, one can tell the computer to execute several hundreds of runs by selecting a range of values for each parameters and the number of cores to be used for the calculations. The ensemble_run.sh will create a number scripts (same number as the selected cpu cores) to be executed on a single core. The scipts created by ensemble_run.sh make sure that there will never be more than one run being executed by any processor. 
+For instance, one can tell the computer to execute several hundreds of runs by selecting a range of values for each 
+parameters and the number of cores to be used for the calculations. The ensemble_run.sh will create a number scripts 
+(same number as the selected cpu cores) to be executed on a single core. The scipts created by ensemble_run.sh make 
+sure that there will never be more than one run being executed by any processor. 
 
-Alternatively one can give a range of values for each parameter and let the computer radnomly select the values of the parameters within the range provided. Currently the random values are selected from uniform distribution of parameter values, but in the future I will add the option to select teh values from gaussian distributions.
+Alternatively one can give a range of values for each parameter and let the computer radnomly select the values of 
+the parameters within the range provided. Currently the random values are selected from uniform distribution of 
+parameter values, but in the future I will add the option to select teh values from gaussian distributions.
 
 # ------------------------------------------------------------------------------------------------------------------------------------------
 END
